e7702320cffb094c7b6c87f8a6c7481d1744d1a5
Sirius Kim <jmoumnet@gmail.com>
2020-02-07 13:44:48 +0900

    200206 aius simple_perceptron complete

--- simple_perceptron/perceptron.py
+++ simple_perceptron/perceptron.py
@@ -58,3 +58,26 @@ class Perceptron(object):
 
 	def predict(self, X):
 		return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+""" Practice code """
+import os
+import pandas as pd
+df = pd.read_csv('./iris.data', header=None, encoding='utf-8')
+
+import matplotlib.pyplot as plt
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0,2]].values
+# plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+# plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+# plt.xlabel('sepal length [cm]')
+# plt.ylabel('petal length [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
+
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+# plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
+# plt.xlabel('Epochs')
+# plt.ylabel('Num of updates')
+# plt.show()
